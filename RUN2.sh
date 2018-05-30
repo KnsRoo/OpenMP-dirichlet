da30ff1787435c2c59123831995a97e3b88e27ef
KnsRoo <knsroo@gmail.com>
2018-05-30 18:00:35 +0300

    Add files via upload

--- RUN2.sh
+++ RUN2.sh
@@ -8,10 +8,10 @@
 #SBATCH --time=15:00
 #SBATCH --mem-per-cpu=100
 
-max_threads=4
+max_threads=20
 step=1
 
-dim=(100 200)
+dim=(100 200 500 1000)
 for ((omp_threads=1;omp_threads<=$max_threads;omp_threads+=step))
 do
 	export OMP_NUM_THREADS=$omp_threads
